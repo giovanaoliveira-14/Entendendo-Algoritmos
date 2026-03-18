ad3f19d7cef6c5f0e8e62d246d122aed083581dd
Giovana Oliveira <61835026+giovanaoliveira-14@users.noreply.github.com>
2026-03-18 16:41:55 +0000

    feat: exercicio 1.1 e 1.2 e tópico sobre tempo de execução

---
+++ Capitulo-1/exercicios.md
@@ -0,0 +1,9 @@
+## Exercicios 
+
+1.1 Suponha que você tenha uma lista com 128 nomes e esteja fazendo uma pesquisa binária. Qual seria o número máximo de etapas que você levaria para encontrar o nome desejado?
+
+R: Log de 128 na base 2 (Quantos 2s conseguimos multiplicar para chegar a 128?) =>  7 (Número máximo de etapas para encontrar o nome)
+
+1.2 Suponha que você duplique o tamanho da lista. Qual seria o número máximo de etapas agora?
+
+R: 128 x 2 = 256 (Nossa lista tem o tamanho de 256); Log de 256 na base 2 (Quantos 2s conseguimos multiplicar para chegar a 256?) =>  8 (Número máximo de etapas para encontrar o nome)
--- Capitulo-1/pesquisa_binaria.py
+++ Capitulo-1/pesquisa_binaria.py
@@ -7,22 +7,46 @@
 # - A pesquisa binária só funciona quando a lista está ordenada
 
 def pesquisa_binaria(lista, item):
+    # Baixo e alto acompanham a parte da lista que você está procurando
     baixo = 0
     alto = len(lista) - 1
     
-    while baixo <= alto:
-        meio = (baixo + alto) // 2
+    while baixo <= alto: # Enquanto ainda não conseguiu chegar a um único elemento...
+        meio = (baixo + alto) // 2 # verifica o elemento central
         chute = lista[meio]
-        if chute == item:
+        if chute == item: # Acha o item
             return meio
-        if chute > item:
+        if chute > item: # O chute foi muito alto
             alto = meio - 1
-        else:
+        else: # O chute foi muito baixo
             baixo = meio + 1
-    return None
+    return None # O item não existe
 
-minha_lista = [1, 3, 5, 7, 9]
-print (pesquisa_binaria(minha_lista, 3))
-print (pesquisa_binaria(minha_lista, -1))
+minha_lista = [1, 3, 5, 7, 9] # Exemplo de lista
+
+print (pesquisa_binaria(minha_lista, 3)) # => 1
+print (pesquisa_binaria(minha_lista, -1)) # => None
         
 
+# Tempo de execução
+
+# Pesquisa simples é executado em tempo linear
+# Pesquisa binária é executada com tempo logarítmico
+
+#----------------------------------------------
+#    Pesquisa Simples | Pesquisa Binária
+#        100 items    |    100 items
+#            =        |       = 
+#        100 etapas   |    7 etapas
+#----------------------------------------------
+# 4 bilhões de items  |    4 Bilhões de items
+#    =                |        =
+# 4 bilhões de etapas |     32 etapas
+#----------------------------------------------
+# 0(n)                |       0(Log n)
+#----------------------------------------------
+
+# 100 elementos, cada elemento 1 ms, logo, 100ms na pesquisa simples
+# 100 elemento, cada elemento 1 ms, 7ms na pesquisa binária
+# 1 bilhão de elementos, cada elemento 1ms, logo, 11,57 dias na pesquisa simples
+# 1 bilhão de elementos, cada elemento 1ms, logo, 30ms na pesquisa binária
